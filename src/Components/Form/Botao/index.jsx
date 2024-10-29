364369c7e2fa451a802901050a4901b24f7ed297
americanlucas <lucasamericanov8@gmail.com>
2024-10-29 11:41:22 -0300

    Glassmorphism e Js no Botão
    Adicionado glassmorphism no formulário e adicionado função no botão onde muda o fundo a medida em que é clicado

--- src/Components/Form/Botao/index.jsx
+++ src/Components/Form/Botao/index.jsx
@@ -1,9 +1,30 @@
+import { useState } from "react";
 import "./style.css"
 
 const Botao = (props) => {
+    const [clicado, setClicado] = useState(false); // Estado para verificar se foi clicado
+
+    function aoClicar() {
+        document.getElementById('btn').style.backgroundColor = "green";
+        setClicado(true); // Define o estado como clicado
+    }
+
+    function desclicar() {
+        document.getElementById('btn').style.backgroundColor = "#2A483C";
+        setClicado(false); // Define o estado como não clicado
+    }
+
+    function handleClick() {
+        if (clicado) {
+        desclicar();
+        } else {
+        aoClicar();
+        }
+    }
+
     return (
         <>
-            <button className="botao">{props.botao}</button>
+            <button onClick={handleClick} id="btn" className="botao">{props.botao}</button>
         </>
     )
 }
